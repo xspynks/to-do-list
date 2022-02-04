c10d89ba991b483c054f42c0b3f80d0a73e05ead
Rafael de Paula <xspynks@gmail.com>
2022-02-04 01:14:34 -0300

    first commit :)

---
+++ main.js
@@ -0,0 +1,68 @@
+loadEvents();
+// loead every event in the page
+function loadEvents(){
+
+    document.querySelector('form').addEventListener('submit', submit);
+    document.getElementById('clear').addEventListener('click', clearList);
+    document.querySelector('ul').addEventListener('click', deleteOrTick);
+    
+
+}
+
+
+
+// submit data function
+function submit(e){
+    e.preventDefault();
+    let taskList;
+    let input = document.querySelector('input');
+    if (input.value != '')
+    addTask(input.value);
+    input.value = '';
+}
+
+
+// add tasks
+function addTask(task){
+    let ul = document.querySelector('ul');
+    let li = document.createElement('li');
+    li.innerHTML = `<span class="delete">×</span><input type="checkbox"><label>${task}</label>`;
+    ul.appendChild(li);
+    document.querySelector('.tasksBoard').style.display = 'block';
+}
+    
+
+// clear the list
+function clearList(e){
+  let ul = document.querySelector('ul').innerHTML = '';
+}
+
+// deleteTick
+function deleteOrTick(e){
+  if(e.target.className == 'delete')
+    deleteTask(e);
+  else {
+    tickTask(e);
+  }
+}
+
+// delete task
+function deleteTask(e){
+  let remove = e.target.parentNode;
+  let parentNode = remove.parentNode;
+  parentNode.removeChild(remove);
+}
+
+// tick a task
+function tickTask(e){
+  const task = e.target.nextSibling;
+  if(e.target.checked){
+    task.style.textDecoration = "line-through";
+    task.style.color = "#ff5666";
+  }else {
+    task.style.textDecoration = "none";
+    task.style.color = "#FFF";
+  }
+
+}
+
